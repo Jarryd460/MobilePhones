e7520b89faa22e2558b9c584086884ab03805312
thekableguy <matthew.kable@gmail.com>
2015-10-25 14:41:00 +0200

    Service interface changes

--- src/main/java/za/ac/cput/MobilePhones/services/Services.java
+++ src/main/java/za/ac/cput/MobilePhones/services/Services.java
@@ -1,5 +1,17 @@
 package za.ac.cput.MobilePhones.services;
 
+import java.util.List;
+
 public interface Services<S, ID> {
+
+    public S findById(ID id);
+
+    public S create(S entity);
+
+    public S edit(S entity);
+
+    public void delete(S entity);
+
+    public List<S> findAll();
 
 }
