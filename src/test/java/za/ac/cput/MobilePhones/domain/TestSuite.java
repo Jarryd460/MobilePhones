d7c4f42e8550406632ba415db30577359d35bc8f
Kristen Hide <kristen@drjekyll.za.net>
2015-10-29 23:50:33 +0200

    Added TestSuite

---
+++ src/test/java/za/ac/cput/MobilePhones/domain/TestSuite.java
@@ -0,0 +1,24 @@
+package za.ac.cput.MobilePhones.domain;
+
+        import org.junit.runner.RunWith;
+        import org.junit.runners.Suite;
+
+/**
+ * Created by Kristen on 29/10/2015.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AddressTest.class,
+        ContactTest.class,
+        CustomerTest.class,
+        NameTest.class,
+        OrderProductTest.class,
+        OrdersTest.class,
+        ProductPriceTest.class,
+        ProductTest.class
+})
+public class TestSuite
+{
+}
+
